3ef56f2c0c3206daac0f5622d4dbe28d9c064c4b
Liken <likentan@gmail.com>
2017-07-23 07:51:25 +0800

    Added solution for Challenge 3-3.

--- fuel_injection_perfection/solution.py
+++ fuel_injection_perfection/solution.py
@@ -1,2 +1,21 @@
+table = {1: 0, 2: 1}
+
+
 def answer(n):
     # your code here
+    n = int(n)
+
+    return recursion(n)
+
+
+def recursion(n):
+    if n in table:
+        return table[n]
+
+    if n % 2 != 0:
+        table[n] = min(recursion((n + 1) / 2) + 2,
+                       recursion((n - 1) / 2) + 2)
+    else:
+        table[n] = recursion(n / 2) + 1
+
+    return table[n]
